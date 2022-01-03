52a1ff6356586b7e0e436b10ac8a90b49761a407
wi100wi <arin.birko@yandex.ru>
2022-01-04 00:21:58 +0500

    Класс

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -13,25 +13,17 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
+            Point p1 = new Point();
 
-            Draw(x1, y1, c1);
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
 
-            int x2 = 3;
-            int y2 = 4;
-            char c2 = '#';
-            Draw(x2, y2, c2);
+            p1.Draw();
+
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-
-            Console.Write(c);
-        }
     }
 }
