6e97389862b02209e1e813ebf59600a91ee64ef8
wi100wi <arin.birko@yandex.ru>
2022-01-03 23:55:26 +0500

    Попытка 1

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -10,6 +10,28 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Drow(x1, y1, c1);
+
+            int x2 = 3;
+            int y2 = 4;
+            char c2 = '#';
+            Drow(x2, y2, c2);
+
+            Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+
+            Console.Write(c);
         }
     }
 }
